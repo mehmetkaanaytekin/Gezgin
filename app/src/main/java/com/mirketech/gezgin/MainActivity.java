0df2bb4bfb99037d9cfac1a31e183a9cd60e9da9
Yasin Yusuf Avcı <yasinyusufavci@gmail.com>
2016-03-31 14:21:07 +0300

    test

--- app/src/main/java/com/mirketech/gezgin/MainActivity.java
+++ app/src/main/java/com/mirketech/gezgin/MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        //yusuf test
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
