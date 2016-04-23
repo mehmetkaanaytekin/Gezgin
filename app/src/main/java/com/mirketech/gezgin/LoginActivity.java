964b84183fc43679b5e368f4447235e5f27216b3
kaytekin <kaanaytekin87@hotmail.com>
2016-04-23 14:36:50 +0300

    Icon changes and login_activity.xml design

--- app/src/main/java/com/mirketech/gezgin/LoginActivity.java
+++ app/src/main/java/com/mirketech/gezgin/LoginActivity.java
@@ -67,8 +67,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+        //mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        /*populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -91,7 +91,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        mProgressView = findViewById(R.id.login_progress);*/
     }
 
     private void populateAutoComplete() {
